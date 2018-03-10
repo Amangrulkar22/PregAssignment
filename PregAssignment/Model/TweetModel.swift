be25b7f7eadd0a847ffe671a27389f83929da572
amangrulkar22 <amangrulkar22@gmail.com>
2018-03-10 13:00:22 +0530

    Initial project commit

---
+++ PregAssignment/Model/TweetModel.swift
@@ -0,0 +1,20 @@
+//
+//  TweetModel.swift
+//  PregAssignment
+//
+//  Created by Ashwinkumar Mangrulkar on 10/03/18.
+//  Copyright © 2018 Ashwinkumar Mangrulkar. All rights reserved.
+//
+
+import Foundation
+
+struct TweetModel {
+    
+    var id: String = ""
+    var title: String = ""
+    var discription: String = ""
+    var prifilePic: String = ""
+    var banner: String = ""
+    var favCount: Int = 0
+    var retweetCount: Int = 0
+}
